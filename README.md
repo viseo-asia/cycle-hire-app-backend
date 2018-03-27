1789d1abb032ec79363ae2a74e73368048f68458
Jeffrey Marvin Forones <jeffrey.forones@viseo.com>
2018-03-27 15:35:14 +0800

    enable cors, installed expressjs, added offline start plugin, change node env from 6.10 to 8.10

---
+++ README.md
@@ -0,0 +1,8 @@
+# AWS credential setup URL
+https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
+
+# Deploy to AWS lambda
+sls deploy --region ap-southeast-1
+
+# run to local server
+sls offline start
---
+++ index.js
@@ -0,0 +1,31 @@
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch')
+const DataLoader = require('dataloader')
+
+const bikepointResource = async (keys) => 
+    Promise.all(keys.map(async (key) => {
+        const response = await fetch('https://api.tfl.gov.uk/bikepoint')
+        return response.json()
+    }))
+
+app.use(bodyParser.json({ strict: false }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/bikepoint', (req, res) => {
+    const bikepointLoader = new DataLoader(async (keys) => await bikepointResource(keys))
+    bikepointLoader.load(0)
+    .then(data => res.send(data))
+})
+
+module.exports.handler = serverless(app);
