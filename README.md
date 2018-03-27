26856ad4f681d273278e8395ccd1561e7a9b8a40
Jeffrey Marvin Forones <jeffrey.forones@viseo.com>
2018-03-27 16:26:20 +0800

    configure gitignore and remove async await implementation since it's latest support is only node version 6.10

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
 # Deploy to AWS lambda
 sls deploy --region ap-southeast-1
 
+> Use this method when you have updated your Function, Event or Resource configuration in serverless.yml and you want to deploy that change (or multiple changes at the same time) to Amazon Web Services.
+
+`Note: You can always enforce a deployment using the --force option.`
+
 # run to local server
 sls offline start
 
--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const bodyParser = require('body-parser');
 const fetch = require('node-fetch')
 const DataLoader = require('dataloader')
 
-const bikepointResource = async function (keys) {
-    return Promise.all(keys.map(async (key) => {
-        const response = await fetch('https://api.tfl.gov.uk/bikepoint')
-        return response.json()
-    }))
-}
+const bikepointResource = (keys) =>
+    Promise.all(keys.map((key) => fetch('https://api.tfl.gov.uk/bikepoint')
+    .then(r => r.json())))
 
 app.use(bodyParser.json({ strict: false }));
 app.use(function(req, res, next) {
@@ -19,12 +16,12 @@ app.use(function(req, res, next) {
     next();
   });
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
   res.send('Hello World!')
 })
 
-app.get('/bikepoint', (req, res) => {
-    const bikepointLoader = new DataLoader(async (keys) => await bikepointResource(keys))
+app.get('/bikepoint', function(req, res) {
+    const bikepointLoader = new DataLoader((keys) => bikepointResource(keys))
     bikepointLoader.load(0)
     .then(data => res.send(data))
 })
