6a290ff6fc5c785a092d7a3b3f02544f9eb7be4d
Jeffrey Marvin Forones <jeffrey.forones@viseo.com>
2018-03-28 14:25:41 +0800

    add appropriate server fault if error

--- modules/bikepoint-module/index.js
+++ modules/bikepoint-module/index.js
@@ -3,12 +3,12 @@ const {bikepointLoader, bikepointFindById, bikepointSearch} = require('./model')
 const list = (req, res) => 
     bikepointLoader()
     .then(data => res.send({data, message: "Results found"}))
-    .catch(error => res.send(error))
+    .catch(error => res.status(500).send(error))
 
 const findOne = (req, res) => 
     bikepointFindById(req.params.id)
     .then(data => res.send({data, message: "Results found"}))
-    .catch(error => res.send(error))
+    .catch(error => res.status(500).send(error))
 
 const search = (req, res) => 
     bikepointSearch(req.query.query)
@@ -16,13 +16,15 @@ const search = (req, res) =>
         let message
         if(!!data.length) {
             message = "Results found"
+            res.status(200)
         } else {
             message = `Cannot find results for keyword ${req.query.query}`
+            res.status(404)
         }
         return {data, message}
     })
     .then(response => res.send(response))
-    .catch(error => res.send(error))
+    .catch(error => res.status(500).send(error))
 
 module.exports = {
     requestHandler: {
