1b4cf21adb312fdfdf2aadb016333a5b56971506
Jeffrey Marvin Forones <jeffrey.forones@viseo.com>
2018-03-28 11:53:38 +0800

    refactor fetch query to curry function and return as a helper

---
+++ helpers/bikepoint-fetch-handler.js
@@ -0,0 +1,9 @@
+const RequestHandler = (baseUrl = "https://api.tfl.gov.uk") => ({
+    bikepoint: (bikePointUrl = "BikePoint") => ({
+        list: () => baseUrl + "/" + bikePointUrl + '/',
+        findOneById: (id) => baseUrl + "/" + bikePointUrl + '/' + id,
+        search: (query) => baseUrl + "/" + bikePointUrl + '/Search?query=' + query
+    })
+})
+
+module.exports = {RequestHandler}
--- modules/bikepoint-module/model.js
+++ modules/bikepoint-module/model.js
@@ -1,8 +1,11 @@
 const fetch = require('node-fetch')
 const DataLoader = require('dataloader')
+const {RequestHandler} = require('../../helpers/bikepoint-fetch-handler')
+
+const api = RequestHandler()
 
 const bikepointResource = (keys) =>
-    Promise.all(keys.map((key) => fetch('https://api.tfl.gov.uk/bikepoint')
+    Promise.all(keys.map((key) => fetch(api.bikepoint().list())
     .then(r => r.json())))
 
 const bikepointLoader = () => {
@@ -11,7 +14,7 @@ const bikepointLoader = () => {
 }
 
 const bikepointFindById = (id) =>
-    fetch(`https://api.tfl.gov.uk/bikepoint/${id}`)
+    fetch(api.bikepoint().findOneById(id))
     .then(r => r.json())
 
 module.exports = {
